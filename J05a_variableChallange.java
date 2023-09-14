9d04673bf663adb424835ba79e72497461c4bcd3
qpamuel <102954408+qpamuel@users.noreply.github.com>
2023-09-14 11:23:09 +0200

    Add files via upload

---
+++ J05a_variableChallange.java
@@ -0,0 +1,13 @@
+package com.example.basics.data.types;
+
+public class J05a_variableChallange {
+    public static void main(String[] args) {
+        byte quantyOWheels = 5;
+        short numberOfParts = 24934;
+        int kilomitersDriven = 3224023;
+        long quantyOfAtoms = 23456645421234l;
+        float olieVolume = 2.35343f;
+        double fuelVolume = 54.2345523453d;
+        boolean firstOwner = true;
+    }
+}
---
+++ J10a_arrayChallange.java
@@ -0,0 +1,12 @@
+package com.example.basics.data.types;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class J10a_arrayChallange {
+    public static void main(String[] args) {
+        String names[] = { "Ania", "Ola", "Daniel", "Karol", "Kasia"};
+
+        BufferedReader reader = new BufferedReader( new InputStreamReader(System.in));
+    }
+}
