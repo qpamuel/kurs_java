f7a2ab431049fb455044188f384551aef0336600
Samuel Sokołowski <102954408+qpamuel@users.noreply.github.com>
2023-10-11 19:44:33 +0200

    Add files via upload

--- J18_Operators1_basics.java
+++ J18_Operators1_basics.java
@@ -16,32 +16,32 @@ public class J18_Operators1_basics {
         System.out.println("modulo 20 % 7: " + (20 % 7));
 
 
-            // Operatory pozwalają na wykonanie różnych operacji na danych np obliczeń matematycznych.
+        // Operatory pozwalają na wykonanie różnych operacji na danych np obliczeń matematycznych.
 
-            //Operator przypisania pozwala na przypisanie wartości do zmiennej czy stałej.
-            int num = 10; // 10
+        //Operator przypisania pozwala na przypisanie wartości do zmiennej czy stałej.
+        int num = 10; // 10
 
-            //Operator przypisania jako jedyny z operatorów dwuargumentowych ma wiązanie prawostronne.
-            //Poniższe wyrażenie jest obliczane od prawej do lewej.
-            int value = num + 90; // 100
-            int a, b;
+        //Operator przypisania jako jedyny z operatorów dwuargumentowych ma wiązanie prawostronne.
+        //Poniższe wyrażenie jest obliczane od prawej do lewej.
+        int value = num + 90; // 100
+        int a, b;
 
-            // priorytet + jest wyższy od =, pierwsze będzie obliczone wyrażenie num + value
-            // wynik będzie podstwiony do zmiennej a oraz b
-            a = b = num + value; // a, b = 110
-            System.out.println( "a:" + a + " b:" + b ); //a:110 b:110
+        // priorytet + jest wyższy od =, pierwsze będzie obliczone wyrażenie num + value
+        // wynik będzie podstwiony do zmiennej a oraz b
+        a = b = num + value; // a, b = 110
+        System.out.println( "a:" + a + " b:" + b ); //a:110 b:110
 
-            // Operatory arytmetyczne służą do operacji na liczbach.
+        // Operatory arytmetyczne służą do operacji na liczbach.
 
-            int i1 = 3 + 5; // wynik dodawania: 8
-            int i2 = 9 - i1; // wynik odejmowania: 1
-            int i3 = 2 * 2; // wynik mnożenia: 4
-            int i4 = 10 / 4; // wynik dzielenia całkowitoliczbowego to 2
-            float f1 = 10 / 2.5f; //wynik dzielenia: 4.0
-            int rest = 12 % 8; // modulo, reszta z dzielenia: 4
+        int i1 = 3 + 5; // wynik dodawania: 8
+        int i2 = 9 - i1; // wynik odejmowania: 1
+        int i3 = 2 * 2; // wynik mnożenia: 4
+        int i4 = 10 / 4; // wynik dzielenia całkowitoliczbowego to 2
+        float f1 = 10 / 2.5f; //wynik dzielenia: 4.0
+        int rest = 12 % 8; // modulo, reszta z dzielenia: 4
 
-            // Kolejność wykonywania działań jest taka sama jak w matematyce.
-            int exampleOfValue = 10 - 2 * 2; // wynik 6
+        // Kolejność wykonywania działań jest taka sama jak w matematyce.
+        int exampleOfValue = 10 - 2 * 2; // wynik 6
 
     }
 }
