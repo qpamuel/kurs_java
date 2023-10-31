e92cef87eaf21a0fc278b5ac79716b2d14e52a12
Samuel Sokołowski <102954408+qpamuel@users.noreply.github.com>
2023-10-31 10:05:47 +0100

    Add files via upload

---
+++ J46_Continue.java
@@ -0,0 +1,25 @@
+package com.example.basics.control_block_statements;
+
+public class J46_Continue {
+    public static void main(String[] args) {
+
+        // countinue - wyjście z pętli
+        // Jeśli potrzebne jest przerwanie aktualnej iteracji pętli, ale nie całej pętli -
+        // stosuje się instruckje continue
+
+        for(int a = 0; a <= 5; a++) {
+
+            if(a == 3) {
+                // przerwanie aktualnej iteracji pętli - 3 nie będzie wyświetlone
+                continue;
+            }
+            System.out.println(a);
+        }
+
+        for(int b = 0; b < 10; b++) {
+            if(b == 2 || b == 5 || b == 9) continue;
+            System.out.println("Wartość b: " + b);
+        }
+
+    }
+}
